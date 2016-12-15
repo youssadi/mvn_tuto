27a39b256e0fb33228d5a733152eff9d98673a73
youssef <saadi>
2016-12-15 19:46:06 +0000

    upload

---
+++ src/main/java/com/youssadi/Calcul.java
@@ -0,0 +1,8 @@
+package com.youssadi;
+
+public class Calcul {
+	public int add(int a, int b){
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/youssadi/CalculTest.java
@@ -0,0 +1,20 @@
+package com.youssadi;
+
+import junit.framework.TestCase;
+
+public class CalculTest extends TestCase {
+    private Calcul cl;
+	protected void setUp() throws Exception {
+		super.setUp();
+		cl = new Calcul();
+	}
+	
+	public void testAdd(){
+		assertTrue(3==cl.add(4, 1));
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
